01be5b68a9e33c907df3cc2aede6bbb2b9e29305
Maija2024 <maija.nieminen@student.hamk.fi>
2024-11-22 18:49:59 +0200

    Autot, kohdat 1-3

--- Auto -Git/src/App.java
+++ Auto -Git/src/App.java
@@ -3,3 +3,30 @@ public class App {
         
     }
 }
+// Kohdat 1 ja 2. Luodaan Auto-niminen luokka ja määritellään sille attribuutit.
+class Auto
+{
+ public String merkki;
+ public String malli;
+ public int bensanMaara;
+
+
+// Kohta 3 a-c. Määritellään metodit.
+  public void Jarruta()
+{
+     System.out.println("Auto jarruttaa");
+}
+  public void Kiihdyta()
+{
+    bensanMaara = bensanMaara -1;
+    System.out.println("Auto kiihtyy");
+}
+ public void NaytaTiedot()
+{
+    System.out.println("Auton merkki on: " + merkki);
+    System.out.println("Auton malli on: " + malli);
+    System.out.println("Bensan määrä on: " + bensanMaara);
+}
+}
+ 
+
