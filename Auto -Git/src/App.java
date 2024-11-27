d2d9a67565db42547afa0220fb929e7032b579c0
saijalind <saija.lind@student.hamk.fi>
2024-11-27 11:31:06 +0200

    Lisätty koodia
    Lisätty tehtävän kohdat 4.-6.
    Olion luonti
    Tankkaa metodi
    Kiihdytä metodiin lisätty ehto

--- Auto -Git/src/App.java
+++ Auto -Git/src/App.java
@@ -16,17 +16,45 @@ class Auto
 {
      System.out.println("Auto jarruttaa");
 }
-  public void Kiihdyta()
-{
-    bensanMaara = bensanMaara -1;
+//Lisätään ehto "jos bensan määrä enemmän kuin 0, auto kiihtyy"
+  public void Kiihdyta() {
+    if (bensanMaara > 0) {
     System.out.println("Auto kiihtyy");
-}
+    bensanMaara = bensanMaara -1;
+}}
  public void NaytaTiedot()
 {
     System.out.println("Auton merkki on: " + merkki);
     System.out.println("Auton malli on: " + malli);
     System.out.println("Bensan määrä on: " + bensanMaara);
 }
-}
+
+//Kohta 6. tankkaa -metodi
+
+public void tankkaa(int maara) {
+  System.out.println("Tankissa bensaa: " + bensanMaara);
+  System.out.println("Tankkaus: " + maara);
+  bensanMaara += maara; 
+  System.out.println("Tankissa bensaa tankkauksen jälkeen: " + bensanMaara);
+
+  
+  // Kohta. 4 luodaan ensin Olio auto1, tulostetaan tiedot, kiihdytetään ja tulostetaan uudestaan.
+
+  Auto auto1 = new Auto();
+  auto1.merkki = "Kia";
+  auto1.malli = "Stonic";
+  auto1.bensanMaara = 5;
+
+  System.out.println("Auton tiedot:");
+  auto1.NaytaTiedot();
+
+  auto1.Kiihdyta();
+
+  System.out.println("Auton tiedot kiihdytyksen jälkeen:");
+  auto1.NaytaTiedot();
+
+
+
+}}
  
 
