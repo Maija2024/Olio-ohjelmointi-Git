5ac7a2883bd42f51b84bb1e687c2e3414525e1ab
Maija2024 <maija.nieminen@student.hamk.fi>
2024-11-29 16:59:48 +0200

    Autot, Muokattu hieman kohtia 4 ja 6. Lisätty kommentti osoittamaan kohtaa 5. Tehty kohta 7, muodostimet.

--- Autot, muokattu/src/App.java
+++ Autot, muokattu/src/App.java
@@ -1,14 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-          // Kohta. 4 luodaan ensin Olio auto1, tulostetaan tiedot, kiihdytetään 
-          //ja tulostetaan uudestaan.
-          // Muokataan kohta 4 mainin yläpuolelle.
-
-  Auto auto1 = new Auto();
+        Auto auto1 = new Auto();
   auto1.merkki = "Kia";
   auto1.malli = "Stonic";
   auto1.bensanMaara = 5;
-  
+
   System.out.println("Auton tiedot:");
   auto1.NaytaTiedot();
 
@@ -16,8 +12,6 @@ public class App {
 
   System.out.println("Auton tiedot kiihdytyksen jälkeen:");
   auto1.NaytaTiedot();
-
-  auto1.tankkaa(10);
     }
 }
 // Kohdat 1 ja 2. Luodaan Auto-niminen luokka ja määritellään sille attribuutit.
@@ -27,27 +21,13 @@ class Auto
  public String malli;
  public int bensanMaara;
 
- // Kohta 7. Luodaan parametriton ja parametrillinen muodostin.
-
- public Auto()
- {
-  merkki = "";
-  malli = "";
-  bensanMaara = 0;
- }
- public Auto (String me, String ma, int bensanMaara)
- {
-  merkki = me;
-  malli = ma;
-  this.bensanMaara = bensanMaara;
- }
 
 // Kohta 3 a-c. Määritellään metodit.
   public void Jarruta()
 {
      System.out.println("Auto jarruttaa");
 }
-//Kohta 5. Lisätään ehto "jos bensan määrä enemmän kuin 0, auto kiihtyy"
+//Lisätään ehto "jos bensan määrä enemmän kuin 0, auto kiihtyy"
   public void Kiihdyta() {
     if (bensanMaara > 0) {
     System.out.println("Auto kiihtyy");
@@ -60,21 +40,31 @@ class Auto
     System.out.println("Bensan määrä on: " + bensanMaara);
 }
 
-//Kohta 6. tankkaa -metodi. 
-// Muokattu bensanMaara-parametria.
+//Kohta 6. tankkaa -metodi
 
 public void tankkaa(int maara) {
   System.out.println("Tankissa bensaa: " + bensanMaara);
   System.out.println("Tankkaus: " + maara);
-  bensanMaara = maara+ bensanMaara; 
+  bensanMaara += maara; 
   System.out.println("Tankissa bensaa tankkauksen jälkeen: " + bensanMaara);
 
   
+  // Kohta. 4 luodaan ensin Olio auto1, tulostetaan tiedot, kiihdytetään ja tulostetaan uudestaan.
+
+//  Auto auto1 = new Auto();
+//   auto1.merkki = "Kia";
+//   auto1.malli = "Stonic";
+//   auto1.bensanMaara = 5;
+
+//   System.out.println("Auton tiedot:");
+//   auto1.NaytaTiedot();
+
+//   auto1.Kiihdyta();
+
+//   System.out.println("Auton tiedot kiihdytyksen jälkeen:");
+//   auto1.NaytaTiedot();
 
 
 
-
-}
-}
- 
-
+}}
+  
